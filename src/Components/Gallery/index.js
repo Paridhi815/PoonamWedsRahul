0b179e853841dcd673d4c6dfa63a1db88ca87aaa
Digital Workbench <mdl.digital.workbench@gmail.com>
2018-12-26 22:52:12 +0530

    Images added for gallery and gallery component made.

--- src/Components/Gallery/index.js
+++ src/Components/Gallery/index.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
-import styles from './style.css';
+import './style.css';
+// import pictures from '../../Assets/Images';
 
 export default class Gallery extends Component {
-  render() {
-      return(
-          <div></div>
-      );
-  }
+    render() {
+        const imageList = ['3.jpg', '1.jpg', '2.jpg', '4.jpg', '6.jpg', '5.jpg', '7.jpg', '9.jpg', '8.jpg'];
+        return (
+            <div className='Gallery-container'>
+                <div className="image-container">
+                    {
+                        imageList.map(imageName => <img className="image-holder" src = {require(`../../Assets/Images/${imageName}`)}
+                            alt="Pre-Wedding Pictures" ></img>)
+                    }
+                </div>
+            </div>
+        );
+    }
 }
