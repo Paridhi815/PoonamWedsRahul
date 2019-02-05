ade2bc81b62b4778070a23af44fafdd323407ca2
Paridhi815 <pari815@gmail.com>
2019-02-05 22:42:31 +0530

    Made quotes according to di.

--- src/Components/KnowUs/index.js
+++ src/Components/KnowUs/index.js
@@ -25,7 +25,7 @@ export default class KnowUs extends Component {
         });
     }
 
-    twoPiks = (imageName1, imageName2, imageDescription) => {
+    twoPiks = (imageName1, imageName2, imageHeading, imageDescription) => {
         return (
             <div className="memories-image-two">
                 <div>
@@ -37,7 +37,9 @@ export default class KnowUs extends Component {
                 </div>
                 <div className="image-description">
                     <span>
-                        {imageDescription}
+                        <div className="image-heading">{imageHeading}</div>
+                        <hr className="image-hr"></hr>
+                        <div className="image-write">{imageDescription}</div>
                     </span>
                 </div>
                 <div>
@@ -51,7 +53,7 @@ export default class KnowUs extends Component {
         )
     }
 
-    onePik = (imageName,imageDescription) => {
+    onePik = (imageName, imageHeading, imageDescription) => {
         return (<div className="memories-image">
             <div>
                 <img className="funny-image-holder" src={require(`../../Assets/Images/Funny/Small/${imageName}`)}
@@ -62,7 +64,9 @@ export default class KnowUs extends Component {
             </div>
             <div className="image-description">
                 <span>
-                    {imageDescription}
+                    <div className="image-heading">{imageHeading}</div>
+                    <hr className="image-hr"></hr>
+                    <div className="image-write">{imageDescription}</div>
                 </span>
             </div>
         </div>);
@@ -84,19 +88,19 @@ export default class KnowUs extends Component {
                     <div className="modal-close-button" onClick={this.closeModal}><CloseIcon isNavBar={false} /></div>
                 </Modal>}
                 <div className="memories-image-container">
-                    {this.onePik('33.jpg',"Attended marriages together")}
-                    {this.onePik('34.jpg',"Attended marriages together")}
-                    {this.twoPiks('29.jpg','41.jpg',"ngdfiugik")}
-                    {this.onePik('37.jpg',"Attended marriages together")}
-                    {this.onePik('38.jpg',"Attended marriages together")}
-                    {this.twoPiks('26.jpg','35.jpg',"ngdfiugik")}
-                    {this.onePik('32.jpg',"Attended marriages together")}
-                    {this.onePik('24.jpg',"Attended marriages together")}
-                    {this.twoPiks('25.jpg','27.jpg',"ngdfiugik")}
-                    {this.onePik('28.jpg',"Had unforgettable Trips")}
-                    {this.onePik('23.jpg',"Attended marriages together")}
-                    {this.twoPiks('22.jpg','21.jpg',"Attended marriages together")}
-                    {this.onePik('42.jpg',"Now we join our hands, and with our hands our hearts ❤️.")}
+                    {this.onePik('33.jpg', "The very beginning", "Friends from day 1 of college, discussing our crush with each other and being together in the thick and thin of life, we are best friends for life!")}
+                    {this.onePik('34.jpg', "Hell of a ride", "Fights, clashes and arguments made our relationship stronger and ever lasting!")}
+                    {this.twoPiks('29.jpg', '41.jpg', "Kids Forever!!", "When we are together, we can bring the kids within us as well show the highest level of maturity too 😁")}
+                    {this.onePik('37.jpg', "Foodies Forever", "The visit to mutt, the trecks, the munching in MG Road and the long talks at the back gate will never be forgotten.")}
+                    {this.onePik('38.jpg', "Making memories together", "The last photoshoot of SIT where he looked no less than a hero and she looked cute as always 😉")}
+                    {this.twoPiks('26.jpg', '35.jpg', "Hum Tum", "When he repeated the kurta year after year 😝 and she toiled hard enough to always have a different look.")}
+                    {this.onePik('32.jpg', "Motu Patlu", "Always ready for a pose and hence we have a million pics and a trillion memories together ❤️")}
+                    {this.onePik('24.jpg', "Our Big Day", "Our 1st placement and the excitement, the wish for each other's selection and the rush of emotions during the final announcement.....We have lived it all together 😊")}
+                    {this.twoPiks('25.jpg', '27.jpg', "Birthday Bash 🎂", "The birthday celebrations and the handmade gifts, the cakes and the pani puri treats..Hope we celebrate many more birthdays together 🎁..")}
+                    {this.onePik('28.jpg', "Down The Memory Lane", "Our most memorable trip to Nandi Hills and the perfect weather to complement our spirits and aid to add more and more pics to our collection.")}
+                    {this.onePik('23.jpg', "Travelling together in the journey called LIFE", "When he visited Mahabalipuram and surprised me with one of my favorite gifts from Switzerland. My constant support in all my endeavours.")}
+                    {this.twoPiks('22.jpg', '21.jpg', "Love Aaj Kal", "When d world thought we are no more together, we proved that everlasting  love still exists in the world.")}
+                    {this.onePik('42.jpg', "Lifelines", "Don't know when we became each other's lifelines and soon to be booked for the next 7 janams..Now we join our hands, and with our hands our hearts ❤️.")}
                 </div>
             </div>
         );
